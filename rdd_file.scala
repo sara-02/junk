746f999ac24bcebd81827cfbaf235db9043f2c85
sara-02 <sarahmasud02@gmail.com>
2016-11-30 16:18:50 +0530

    Add elo_rating algo

--- rdd_file.scala
+++ rdd_file.scala
@@ -1,7 +1,5 @@
-val fnames: List[String] = List(<file1>,<file2>...<fileN>)
-var path: String = "hdfs://sandbox:9000/user/root/my_test"
+﻿val fnames: List[String] = List(<file1>,<file2>...<fileN>)
+var path: String = "hdfs://sandbox:9000/user/root/my_test/"
 
-for (fname <- fnames):
-{
- val f = sc.textFile(s"$path/$f").collect(); println(s"File >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>: $f"); f.foreach(println)}
-}
+for (fname <- fnames)
+{ val f = sc.textFile(s"$path$fname").collect(); println(s"File >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>: $f"); f.foreach(println)}
