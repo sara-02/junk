3084acd08f06175568f41c83772eaf3aedd84957
Sarah Masud <sara-02@users.noreply.github.com>
2017-08-16 12:23:02 +0000

    Add classes and constants.

--- find_anchor.py
+++ find_anchor.py
@@ -18,39 +18,56 @@ import json
 import operator
 
 
-def find_highest(filename, threshold=None):
-    if threshold is None:
-        threshold = 20
-    with open(filename, 'r') as f:
-        data = json.load(f)
+class AnchorPackage(object):
 
-    p = data[0]['package_list']
-    l1 = len(p)
-    print l1
-    d = {}
-    for l in p:
-        for e in l:
-            if e in d:
-                d[e] += 1
-            else:
-                d[e] = 1
+    def __init__(self, anchor_list):
+        self.anchor_list = anchor_list
 
-    sorted_x = sorted(d.items(), key=operator.itemgetter(1), reverse=True)
-    sorted_x[0]
-    l2 = len(sorted_x)
-    q = sorted_x[0][1]
-    for i in range(l2):
-        s = sorted_x[i]
-        j = s[1] / l1 * 100
-        j = round(j, 4)
-        if j >= threshold:
-            print s[0] + "        " + str(j) + "            "
+    def read_file(self, filename):
+        with open(filename, 'r') as f:
+            json_data = json.load(f)
+        return json_data
+
+    def write_file(self, filename):
+        with open(filename, 'w') as f:
+            json.dump(self.anchor_list, f, indent=True)
+
+    def find_highest(self, input_filename, output_filename, threshold=None):
+        if threshold is None:
+            threshold = 15
+
+        json_data = self.read_file(input_filename)
+        for data_set in json_data:
+            ecosystem = data_set['ecosystem']
+            anchor_packages = []
+            result_dict = {}
+            package_list = data_set['package_list']
+            total_package_lists = len(package_list)
+            package_name_count = {}
+
+            for dependency_list in package_list:
+                for dependency in dependency_list:
+                    if dependency in package_name_count:
+                        package_name_count[dependency] += 1
+                    else:
+                        package_name_count[dependency] = 1
+            for name, count in package_name_count.items():
+                count_percentage = count / total_package_lists * 100
+                round_off_count = round(count_percentage, 4)
+                if round_off_count >= threshold:
+                    anchor_packages.append((name, round_off_count))
+            result_dict['ecosystem'] = ecosystem
+            result_dict['anchor_packages'] = anchor_packages
+            result_dict['manifest_count'] = total_package_lists
+            self.anchor_list.append(result_dict)
+        self.write_file(output_filename)
+        return
 
 
 def main():
-    find_highest('manifest_vertx.json')
-    find_highest('manifest_springboot.json')
-    find_highest('manifest.json', 10)
+    l = []
+    a = AnchorPackage(l)
+    a.find_highest('/home/smasud/Downloads/manifest.json', 'anchor.json', 15)
 
 if __name__ == '__main__':
     main()
