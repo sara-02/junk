bc1d6541f4f70318167f864a3a7fd20c57db2767
Sarah Masud <sarahmasud02@gmail.com>
2017-09-21 21:13:44 +0000

    Add more fucntionality in the game.

--- word_guess.py
+++ word_guess.py
@@ -1,48 +1,80 @@
 """TODOS:
 * Load list from file.
 * Time out raw_input.
-* Have a score for the game(partial marking alllowed).
 * Make the print statements more decorative.
 * Optimise the string checking algo.
 * Add instructions to play.
-* If a word is guess in the same game, do not repeat it.
-* If the user gusses all words in the same game add a print statment for that.
 * Log and display the time taken for individual word and the time spent on the game.
 * Better expcetion Handling."""
 
 from random import choice
+from timeit import default_timer as timer
+import json
 
 
-def play_game():
-    word_list = ["apple", "mango", "books", "analytics", "games"]
-    random_word = list(choice(word_list))
-    word_len = len(word_list)
-    turns_allowed = word_len + 3
+class WordGuess(object):
 
-    answer_list = ["__ "] * word_len
-    print answer_list
+    def __init__(self, word_list, user="anonymous"):
+        self.user = user
+        self.word_list = word_list
+        self.won = 0
+        self.lost = 0
+        self.score = 0
+        self.word_time_dict = {}
 
-    while(turns_allowed):
-        user_guess = raw_input("Enter a char:: ")
-        for index, char in enumerate(random_word):
-            if user_guess is not None and char == user_guess:
-                answer_list[index] = char
-        print answer_list
-        if answer_list == random_word:
-            print "## You Won! ##"
-            break
-        turns_allowed = turns_allowed - 1
-    else:
-        print "@@ You Lost! @@"
+    def play_game(self):
+        pick_word = choice(self.word_list)
+        guess_word = list(pick_word)
+        guess_word_len = len(guess_word)
+        turns_allowed = guess_word_len + 3
 
+        answer_list = ["__ "] * guess_word_len
+        print(answer_list)
 
-def main():
-    play_game()
-    play_more = raw_input("Enter y to play:: ")
-    while(play_more.lower()[0:1] == "y"):
-        play_game()
+        start_time = timer()
+        while(turns_allowed):
+            user_guess_letter = raw_input("Enter a char:: ")
+            if user_guess_letter is None and not user_guess_letter.isalpha():
+                turns_allowed = turns_allowed - 1
+                continue
+            for index, char in enumerate(guess_word):
+                if char == user_guess_letter:
+                    answer_list[index] = char
+                    self.score += 0.5
+            print(answer_list)
+            if answer_list == guess_word:
+                end_time = timer()
+                print("## You Won! ##")
+                self.score += 2 + turns_allowed
+                self.won += 1
+                break
+            turns_allowed = turns_allowed - 1
+        else:
+            end_time = timer()
+            print("@@ You Lost! @@")
+            self.lost += 1
+            self.score -= 0.3
+        self.word_list.remove(pick_word)
+        self.word_time_dict[pick_word] = end_time - start_time
+
+    def play_again(self):
+        self.play_game()
         play_more = raw_input("Enter y to play:: ")
+        while(play_more.lower()[0:1] == "y" and len(self.word_list) > 0):
+            self.play_game()
+            play_more = raw_input("Enter y to play:: ")
+        if len(self.word_list) == 0:
+            print("END OF GAME: All words exhausted")
+
+        print("Hey %s , well played!" % self.user)
+        print("You played %s games" % str(self.won + self.lost))
+        print("You won %s games" % str(self.won))
+        print("You lost %s games" % str(self.lost))
+        print("Your score is %f" % self.score)
 
 
 if __name__ == '__main__':
-    main()
+    user = "github"
+    word_list = ['sarah', 'masud', 'google']
+    game = WordGuess(word_list, user)
+    game.play_again()
