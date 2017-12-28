bca802c4c221eb82290a0df4f4db68ab44a20ef6
Sarah Masud <sarahmasud02@gmail.com>
2017-12-28 10:21:51 +0530

    Python3 + timer+wordlist load

--- word_guess.py
+++ word_guess.py
@@ -1,26 +1,27 @@
-"""TODOS:
-* Load list from file.
-* Time out raw_input.
-* Make the print statements more decorative.
-* Optimise the string checking algo.
-* Add instructions to play.
-* Log and display the time taken for individual word and the time spent on the game.
-* Better expcetion Handling."""
-
 from random import choice
 from timeit import default_timer as timer
-import json
 
 
 class WordGuess(object):
 
-    def __init__(self, word_list, user="anonymous"):
+    def __init__(self, word_list=[], user=""):
         self.user = user
         self.word_list = word_list
         self.won = 0
         self.lost = 0
         self.score = 0
         self.word_time_dict = {}
+
+    @staticmethod
+    def load_word_list(filename):
+        with open(filename, "r") as f:
+            data = f.readlines()
+        return data
+
+    def clean_list(self, data):
+        for each in data[:-1]:
+            self.word_list.append(each[:-1])
+        self.word_list.append(data[-1])
 
     def play_game(self):
         pick_word = choice(self.word_list)
@@ -32,8 +33,8 @@ class WordGuess(object):
         print(answer_list)
 
         start_time = timer()
-        while(turns_allowed):
-            user_guess_letter = raw_input("Enter a char:: ")
+        while turns_allowed:
+            user_guess_letter = input("Enter a char:: ")
             if user_guess_letter is None and not user_guess_letter.isalpha():
                 turns_allowed = turns_allowed - 1
                 continue
@@ -44,37 +45,47 @@ class WordGuess(object):
             print(answer_list)
             if answer_list == guess_word:
                 end_time = timer()
+                self.word_time_dict[pick_word] = end_time - start_time
                 print("## You Won! ##")
                 self.score += 2 + turns_allowed
                 self.won += 1
                 break
             turns_allowed = turns_allowed - 1
         else:
-            end_time = timer()
             print("@@ You Lost! @@")
             self.lost += 1
             self.score -= 0.3
         self.word_list.remove(pick_word)
-        self.word_time_dict[pick_word] = end_time - start_time
 
     def play_again(self):
         self.play_game()
-        play_more = raw_input("Enter y to play:: ")
+        play_more = input("Enter y to play:: ")
         while(play_more.lower()[0:1] == "y" and len(self.word_list) > 0):
             self.play_game()
-            play_more = raw_input("Enter y to play:: ")
+            play_more = input("Enter y to play:: ")
         if len(self.word_list) == 0:
             print("END OF GAME: All words exhausted")
 
-        print("Hey %s , well played!" % self.user)
-        print("You played %s games" % str(self.won + self.lost))
-        print("You won %s games" % str(self.won))
-        print("You lost %s games" % str(self.lost))
-        print("Your score is %f" % self.score)
+    def print_statements(self, total_time):
+        print(("Hey {} , well played!".format(self.user)))
+        print(("You played {} games".format(self.won + self.lost)))
+        print(("Games Won = {}".format(self.won)))
+        print(("Games lost = {}".format(self.lost)))
+        print(("Your final Score = {}".format(self.score)))
+        print(("Time Spent on Game = {} seconds".format(total_time)))
+        if len(self.word_time_dict) > 0:
+            print("Time spend on each correct guess")
+            for word, time in self.word_time_dict.items():
+                print(("{} : {} seconds").format(word, time))
 
 
 if __name__ == '__main__':
-    user = "github"
-    word_list = ['sarah', 'masud', 'google']
-    game = WordGuess(word_list, user)
+    data = WordGuess.load_word_list("wordlist.txt")
+    game = WordGuess()
+    game.clean_list(data)
+    name = input("Enter your name:: ")
+    game.user = name
+    start_time = timer()
     game.play_again()
+    end_time = timer()
+    game.print_statements(total_time=end_time - start_time)
