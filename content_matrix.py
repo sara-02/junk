19d759b76fab65c8896166510986488e2dcb0e17
Sarah Masud <sarahmasud02@gmail.com>
2018-01-29 18:33:23 +0530

    Update content_matrix.py

--- content_matrix.py
+++ content_matrix.py
@@ -1,8 +1,13 @@
+from __future__ import division
 import pandas as pd
-import json
 import numpy as np
 import scipy.sparse as sp
 import tensorflow as tf
+import json
+
+# Load the specific package list
+with open("package_list_file.json", "r") as f:
+    saved_package_list = set(json.load(f))
 
 # Load the original dataset
 node_details_clean = pd.read_json(
@@ -10,9 +15,8 @@ node_details_clean = pd.read_json(
 
 # generate clean data, fill Nan
 print("generate clean data, fill Nan")
-node_details_trim = node_details_clean[["dependencies", "name", "keywords"]]
-node_details_trim[['dependencies', 'keywords']] = node_details_trim[[
-    'dependencies', 'keywords']].fillna(-1)
+node_details_trim = node_details_clean[
+    ["dependencies", "name", "keywords"]].dropna(how='any')
 print("Delete node details clean")
 del(node_details_clean)
 
@@ -26,8 +30,6 @@ package_dependencies_dict = dict()
 for _, row in node_details_trim.iterrows():
     key_package_name = row["name"].lower()
     values = row["dependencies"]
-    if type(values) == int and values == -1:
-        values = []
     values = [dep_name.lower() for dep_name in values]
     if key_package_name not in package_dependencies_dict:
         package_dependencies_dict[key_package_name] = list(set(values))
@@ -43,8 +45,6 @@ unique_keywords_collection = set()
 for _, row in node_details_trim.iterrows():
     key_package_name = row["name"].lower()
     values_keywords = row["keywords"]
-    if type(values_keywords) == int and values_keywords == -1:
-        values_keywords = []
     values = []
     for name in values_keywords:
         values.append(name.lower())
@@ -64,14 +64,14 @@ del(node_details_trim)
 print("Generate Unique_Keyword_Index_Map and Unique_Package_Name_Index_Map")
 # Generate Unique_Keyword_Index_Map and Unique_Package_Name_Index_Map
 unique_keywords_collection = list(unique_keywords_collection)
-unique_packages_collection = package_dependencies_dict.keys()
+# unique_packages_collection = package_dependencies_dict.keys()
 map_keywords_dict = dict(
     zip(unique_keywords_collection, range(len(unique_keywords_collection))))
 map_packages_dict = dict(
-    zip(unique_packages_collection, range(len(unique_packages_collection))))
+    zip(saved_package_list, range(len(saved_package_list))))
 print("Delete unique key/pck collection")
 del(unique_keywords_collection)
-del(unique_packages_collection)
+# del(unique_packages_collection)
 
 print("Save the Index_Map for future use")
 # Save the Index_Map for future use
@@ -85,13 +85,18 @@ print("Genearate the Package_Aggregated_Tags_Dict")
 # Genearate the Package_Aggregated_Tags_Dict
 content_matrix_dict = dict()
 for package_name, current_tags in package_keywords_dict.items():
+    if package_name not in saved_package_list:
+        continue
     for each_dep in package_dependencies_dict[package_name]:
         tags = package_keywords_dict.get(each_dep, [])
         current_tags.extend(tags)
-    current_tags_index_wise = [map_keywords_dict[keyword]
-                               for keyword in current_tags]
-    content_matrix_dict[map_packages_dict[package_name]] = list(
-        set(current_tags_index_wise))
+    current_tags_index_wise = list(set([map_keywords_dict[keyword]
+                                        for keyword in current_tags]))
+    # Use this to reduce tag index len.
+    # if len(current_tags_index_wise)<5:
+    #     continue
+    content_matrix_dict[map_packages_dict[
+        package_name]] = current_tags_index_wise
 
 print("Delete apckage/dep/key dict")
 del(package_dependencies_dict)
@@ -101,7 +106,7 @@ print("Generate Sparse Matrix using the Package_Aggregated_Tags_Dict")
 # Generate Sparse Matrix using the Package_Aggregated_Tags_Dict
 
 sparse_mat = sp.dok_matrix(
-    (len(map_packages_dict), len(map_keywords_dict)), dtype=np.int64)
+    (len(saved_package_list), len(map_keywords_dict)), dtype=np.int64)
 for package_id, tag_ids in content_matrix_dict.items():
     sparse_mat[package_id, tag_ids] = 1
 
@@ -116,3 +121,8 @@ print("Genearte the Sparse Tensor using Sparse Coo. Matrix")
 # Genearte the Sparse Tensor using Sparse Coo. Matrix
 content_matrix = tf.SparseTensor(indices, sparse_coo.data, sparse_coo.shape)
 print("Size of content matrix = {}".format(content_matrix.get_shape()))
+
+rows, columns = sparse_coo.shape
+total_elements = rows * columns
+sparsity = (total_elements - sparse_coo.getnnz()) / total_elements
+print("Sparsity = {}".format(sparsity))
